091efb969150de4fe75ceb6b8be4e2f8b42c579d
LinnHtet29 <linnh3064@gmail.com>
2024-02-06 16:30:15 +0630

    [add] wroking hours routes add

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,16 @@
-// import express from "express";
+import express from "express";
+import workingHourRoutes from "./workingHour.route";
 // import roomRoutes from "./room.route";
 // import departmentRoutes from "./department.route";
 // import accountRoutes from "./account.route";
 // import appointmentRoutes from "./appointment.route";
 
-// const router = express.Router();
+const router = express.Router();
 
 // router.use("/room", roomRoutes);
 // router.use("/department", departmentRoutes);
 // router.use("/account", accountRoutes);
 // router.use("/appointment", appointmentRoutes);
+router.use("/working_hour", workingHourRoutes)
 
-// export default router;
+export default router;
