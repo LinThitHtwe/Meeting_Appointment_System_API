f558846bcf755c9b2f9ce75e24c3e4cfe8ad5f57
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2024-02-22 09:52:51 +0630

    config For deploy

--- config/db.ts
+++ config/db.ts
@@ -3,23 +3,54 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  "ace_meeting_room_appointment_system",
-  "postgres",
-  process.env.DB_PASSWORD,
-  {
-    dialect: "postgres",
-    host: "localhost",
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+// export const sequelize = new Sequelize(
+//   "ace_meeting_room_appointment_system",
+//   "postgres",
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: "postgres",
+//     host: "localhost",
+//     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     sync: { alter: true },
+//   }
+// );
+
+export const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+  dialect: "postgres",
+  host: PGHOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    sync: { alter: true },
-  }
-);
+    connection: {
+      options: `project=${ENDPOINT_ID}`,
+    },
+  },
+
+  sync: { alter: true },
+});
+
+// const sql = postgres({
+//   host: PGHOST,
+//   database: PGDATABASE,
+//   username: PGUSER,
+//   password: PGPASSWORD,
+//   port: 5432,
+//   ssl: 'require',
+//   connection: {
+//     options: `project=${ENDPOINT_ID}`,
+//   },
+// });
 
 export const testDbConnection = async () => {
   try {
